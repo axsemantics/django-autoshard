6db25b289542bda8b44de3642dc184ed4774617a
Ciprian Tarta <ciprian@cipriantarta.ro>
2016-06-30 17:56:40 +0300

    WIP changes to NODES

--- django_autoshard/factory.py
+++ django_autoshard/factory.py
@@ -7,38 +7,40 @@ from . import utils
 
 
 class ShardingFactory:
+    def __init__(self):
+        self.primary = django_settings.DATABASES.get('default')
 
     def configure(self):
         shards = dict()
-        nodes = django_settings.DATABASES.copy()
-        for node, db in nodes.items():
+        nodes = django_settings.DJANGO_AUTOSHARD.NODES
+        for node in nodes:
             try:
-                _ = db['NAME']
+                _ = node['HOST']
             except KeyError:
-                raise exceptions.ImproperlyConfigured('Node {} does not have a database name.'.format(node))
+                raise exceptions.ImproperlyConfigured('Node {} does not have a host.'.format(node))
             try:
-                _ = db['RANGE']
+                _ = node['RANGE']
             except KeyError:
                 raise exceptions.ImproperlyConfigured('Node {} does not specify a shard range.'.format(node))
 
-            node_shards = self.set_logical_shards(node, db)
+            node_shards = self.set_logical_shards(node)
             shards.update(node_shards)
 
         django_settings.SHARDS = OrderedDict(sorted(shards.items()))
         django_settings.DATABASE_ROUTERS = ('django_autoshard.routers.ShardRouter', )
 
-    def set_logical_shards(self, node, config):
+    def set_logical_shards(self, node):
         result = dict()
-        for i in config['RANGE']:
-            shard = config.copy()
-            del shard['RANGE']
-            shard['NAME'] = '{}_{}'.format(config['NAME'], i)
-            alias = '{}_{}'.format(node, i)
+        for i in node['RANGE']:
+            shard = self.primary.copy()
+            shard['HOST'] = node['HOST']
+            shard['NAME'] = '{}_{}'.format(self.primary['NAME'], i)
+            alias = 'shard_{}'.format(i)
             django_settings.DATABASES[alias] = shard
             node_index = utils.get_shard_index(alias)
-            replicas = self.set_replicas(config)
+            replicas = self.set_replicas()
             result[node_index] = Shard(node_index, alias, replicas)
         return result
 
-    def set_replicas(self, config):
+    def set_replicas(self):
         return {}
